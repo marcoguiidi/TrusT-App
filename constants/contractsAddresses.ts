f8d3b258dc132b1357c2dba797378f3657f70f19
marcoguiidi <marco.guidi24@studio.unibo.it>
2025-07-04 16:09:03 +0200

    update sepolia addresses

--- constants/contractsAddresses.ts
+++ constants/contractsAddresses.ts
@@ -12,8 +12,8 @@ export const chainIdToContractAddresses: {
   [chainId: number]: ContractAddresses;
 } = {
   11155111: {
-    userCompanyRegistry: UserCompanyRegistryContractData.address,
-    tulToken: TulTokenContractData.address,
+    userCompanyRegistry: "0x68e2Fb82Aee7EA0Fb895a7f603fDeAcae4Dd50c3",
+    tulToken: "0x0Ceed6c27616CD4670b1c56f534aC30BC87370bD",
     zoniaContract: "0xfb663f4fc2624366B527c0d97271405D14503121",
   },
   31337: {
